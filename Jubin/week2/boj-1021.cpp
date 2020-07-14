7f04470354c04c27c9729cc713a646de278cf9ea
Jubin Park <jubin-park@naver.com>
2020-07-14 13:45:08 +0900

    Add boj-1021

---
+++ Jubin/week2/boj-1021.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	char N;
+	int M;
+	scanf("%hhd%d", &N, &M);
+	
+	deque<char> dq;
+	for (char n = 1; n <= N; ++n)
+	{
+		dq.push_back(n);
+	}
+	
+	int totalCount = 0;
+	
+	for (int m = 0; m < M; ++m)
+	{
+		char pick;
+		scanf("%hhd", &pick);
+		
+		int count = 0;
+		while (pick != dq.front())
+		{
+			dq.push_back(dq.front());
+			dq.pop_front();
+			++count;
+		}
+		
+		totalCount += min(count, (int)dq.size() - count);
+		dq.pop_front();
+	}
+	
+	printf("%d", totalCount);
+	
+	return 0;
+}
