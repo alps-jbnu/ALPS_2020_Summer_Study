06a3665f5f152bbd18f02ec7269b7a7d3906f6b2
DanielJung <danieltiger60@gmail.com>
2020-07-17 23:49:29 +0900

    week2(mid) solved

--- JB/Day2/5430_IntegerLists/main.cpp
+++ JB/Day2/5430_IntegerLists/main.cpp
@@ -1,38 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
+int T, N;
+string str, token;
+deque<int> deq;
+string parser;
 int main() {
-    int T, N, in;
-    string str, parser;
-    deque<int> deq;
-    //ios::sync_with_stdio(false);
-    //cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     cin >> T;
     while(T--) {
-        cin >> str >> N >> parser;
+        //initialize
+        bool sr = true, isError = false;
         deq.clear();
-        parser = parser.substr(1, parser.length() - 2); // [] 제거
-        string token;
+        // input
+        cin >> str >> N >> parser;
+        // [] 제거
+        parser = parser.substr(1, parser.length() - 2);
+        // tokenize
         stringstream ss(parser);
-        bool sr = true;
-        while(getline(ss, token, ',')) {
+        while (getline(ss, token, ','))
+        {
             deq.push_back(stoi(token));
         }
-        cout << '[';
-        while(!str.empty()) {
-            char next = str[0];
-            str = str.substr(1);
+    
+        for(int i = 0; str[i]; i++) {
+            char next = str[i];
             if(next == 'D') {
-                if(deq.empty()) cout << "error\n";
-                else if(sr) {
-                    cout << deq.front() << ' ';
+                if(deq.empty()) {
+                    cout << "error\n";
+                    isError = true;
+                    break;
+                }
+                else if(sr)
+                    deq.pop_front();
+                else
+                    deq.pop_back();
+            }
+            else
+                sr = !sr;
+        }
+        if (!isError) {
+            cout << '[';
+            while (!deq.empty())
+            {
+                if (sr) {
+                    cout << deq.front();
                     deq.pop_front();
                 }
                 else {
-                    cout << deq.back() << ' ';
+                    cout << deq.back();
                     deq.pop_back();
                 }
+                if (!deq.empty())
+                    cout << ',';
             }
-            else sr = !sr;
+            cout << "]\n";
         }
     }
 }
--- JB/readme.md
+++ JB/readme.md
@@ -16,3 +16,34 @@
 - **결과 값이 `int`범위 인지 `long long`범위인지** 반드시 확인 필요
 - 큐의 크기를 고정하여 Sliding window를 구현할 수 있음
 - `priority_queue`에서 가중치를 음수로 하면 역순으로 구성됨 (ex. 다익스트라)
+- `getline`을 이용한 string tokenizer 구현 방법
+```cpp
+// data : raw_data
+// token : data를 tokenize한 token들을 갖게될 변수
+// delimiter : token 구분자
+vector<string> tokenize_getline(const string& data, const char delimiter = ' ') {
+	vector<string> result;
+	string token;
+	stringstream ss(data);
+
+	while (getline(ss, token, delimiter)) {
+		result.push_back(token);
+	}
+	return result;
+}
+```
+- `strtok`을 이용해서 char[]을 tokenize 가능
+```cpp
+	char str[] = "Geeks-for-Geeks"; 
+
+    // Returns first token  
+    char *token = strtok(str, "-"); 
+
+    // Keep printing tokens while one of the 
+    // delimiters present in str[]. 
+    while (token != NULL) 
+    { 
+        printf("%s\n", token); 
+        token = strtok(NULL, "-"); 
+    } 
+```
