038e49c072c1bacd442c5008e41b24993f52a725
DanielJung <danieltiger60@gmail.com>
2020-08-05 01:06:27 +0900

    14500 solved

--- JB/Week4/14500_TetroNomino/main.cpp
+++ JB/Week4/14500_TetroNomino/main.cpp
@@ -1,42 +1,33 @@
 #include <bits/stdc++.h>
-#define INF 987654321
 using namespace std;
-const int dx[] = { 1, 0, -1, 0 }, dy[] = { 0, 1, 0, -1 };
-
-int N, M, ans;
-bool visited[501][501];
+const int block[][4][2] = {
+	{{0,0},{0,1},{0,2},{0,3}},
+	{{0,0},{1,0},{2,0},{3,0}},
+	{{0,0},{0,1},{1,0},{1,1}},
+	{{1,0},{1,1},{0,1},{1,2}},
+	{{0,0},{1,0},{2,0},{1,1}},
+	{{0,0},{0,1},{0,2},{1,1}},
+	{{1,0},{1,1},{0,1},{2,1}},
+	{{0,0},{1,0},{2,0},{2,1}},
+	{{0,0},{1,0},{0,1},{0,2}},
+	{{0,0},{0,1},{1,1},{2,1}},
+	{{1,0},{1,1},{1,2},{0,2}},
+	{{2,0},{2,1},{1,1},{0,1}},
+	{{0,0},{1,0},{1,1},{1,2}},
+	{{0,0},{0,1},{1,0},{2,0}},
+	{{0,0},{0,1},{0,2},{1,2}},
+	{{0,0},{1,0},{1,1},{2,1}},
+	{{1,0},{1,1},{0,1},{0,2}},
+	{{0,1},{1,0},{1,1},{2,0}},
+	{{0,0},{0,1},{1,1},{1,2}}
+};
 int board[501][501];
-queue<tuple<int, int, int, int> > q;
-int calc(int startX, int startY) { 
-	int ans = 0;
-	visited[startY][startX] = true;
-	q.push({ startX, startY, 3, board[startY][startX] });
-	while (!q.empty()) {
-		int x = get<0>(q.front());
-		int y = get<1>(q.front());
-		int cnt = get<2>(q.front());
-		int sum = get<3>(q.front());
-		
-		if (cnt == 0) break;
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i], ny = y + dy[i];
-			if (nx < 0 || ny < 0 || nx >= M || ny >= N) //|| visited[ny][nx])
-				continue;
-			// visited[ny][nx] = true;
-			q.push({ nx, ny, cnt - 1, sum + board[ny][nx] });
-		}
-	}
-	while (!q.empty()) {
-		ans = max(ans, get<3>(q.front()));
-		q.pop();
-	}
-	return ans;
-}
-
+int ans;
 int main() {
-	// ios::sync_with_stdio(false);
-	// cin.tie(0);
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N, M;
 	cin >> N >> M;
 	for (int y = 0; y < N; y++) {
 		for (int x = 0; x < M; x++) {
@@ -46,8 +37,19 @@ int main() {
 
 	for (int y = 0; y < N; y++) {
 		for (int x = 0; x < M; x++) {
-			memset(visited, false, sizeof(visited));
-			ans = max(ans, calc(x, y));
+			for (int idx = 0; idx < 19; idx++) {
+				int i, sum = 0;
+				for (i = 0; i < 4; i++) {
+					int nx = x + block[idx][i][0];
+					int ny = y + block[idx][i][1];
+					if (nx < 0 || ny < 0 || nx >= M || ny >= N)
+						break;
+					sum += board[ny][nx];
+				}
+				if (i != 4)
+					continue;
+				ans = max(ans, sum);
+			}
 		}
 	}
 	cout << ans;
