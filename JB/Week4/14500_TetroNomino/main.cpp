7b9cbe8fe33cfa3875038080281482c31973f9a3
jjongbumeee <danieltiger60@gmail.com>
2020-08-04 13:45:45 +0900

    14500 solving

--- JB/Week4/14500_TetroNomino/main.cpp
+++ JB/Week4/14500_TetroNomino/main.cpp
@@ -1,22 +1,37 @@
 #include <bits/stdc++.h>
 #define INF 987654321
 using namespace std;
-const int dx[] = { 0, 0, 1, -1 }, dy[] = { 1, -1, 0, 0 };
+const int dx[] = { 1, 0, -1, 0 }, dy[] = { 0, 1, 0, -1 };
 
 int N, M, ans;
 bool visited[501][501];
 int board[501][501];
-int calc(int x, int y, int blocks) { 
-	if (blocks == 0) return 0;
-	if (x < 0 || x >= M || y < 0 || y >= N || visited[y][x]) return -INF;
-	int ret = 0;
-	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i], ny = y + dy[i];
-		visited[y][x] = true;
-		ret = max(ret, calc(nx, ny, blocks - 1) + board[y][x]);
-		visited[y][x] = false;
+queue<tuple<int, int, int, int> > q;
+int calc(int startX, int startY) { 
+	int ans = 0;
+	visited[startY][startX] = true;
+	q.push({ startX, startY, 3, board[startY][startX] });
+	while (!q.empty()) {
+		int x = get<0>(q.front());
+		int y = get<1>(q.front());
+		int cnt = get<2>(q.front());
+		int sum = get<3>(q.front());
+		
+		if (cnt == 0) break;
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i], ny = y + dy[i];
+			if (nx < 0 || ny < 0 || nx >= M || ny >= N) //|| visited[ny][nx])
+				continue;
+			// visited[ny][nx] = true;
+			q.push({ nx, ny, cnt - 1, sum + board[ny][nx] });
+		}
 	}
-	return ret;
+	while (!q.empty()) {
+		ans = max(ans, get<3>(q.front()));
+		q.pop();
+	}
+	return ans;
 }
 
 int main() {
@@ -31,7 +46,8 @@ int main() {
 
 	for (int y = 0; y < N; y++) {
 		for (int x = 0; x < M; x++) {
-			ans = max(ans, calc(x, y, 3));
+			memset(visited, false, sizeof(visited));
+			ans = max(ans, calc(x, y));
 		}
 	}
 	cout << ans;
