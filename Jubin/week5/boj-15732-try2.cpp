c8a142cddcbb7d75c2b9c090f4dfbed57b243023
Jubin Park <jubin-park@naver.com>
2020-08-05 15:20:54 +0900

    Add: boj-15732 도토리 숨기기

---
+++ Jubin/week5/boj-15732-try2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <algorithm>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int N;
+	int K;
+	int D;
+	vector<tuple<int, int, int>> rules;
+	
+	cin >> N >> K >> D;
+	rules.reserve(K);
+	
+	for (int k = 0; k < K; ++k)
+	{
+		int A;
+		int B;
+		int C;
+		cin >> A >> B >> C;
+		rules.push_back(make_tuple(A, B, C));
+	}
+	
+	int low = 1;
+	int high = N;
+	
+	while (low + 1 < high)
+	{
+		int mid = (low + high) / 2;
+		long long count = 0;
+		for (int i = 0; i < K; ++i)
+		{
+			int A = get<0>(rules[i]);
+			int B = get<1>(rules[i]);
+			int C = get<2>(rules[i]);
+			
+			int end = min(B, mid);
+			if (end >= A)
+			{
+				count += (end - A) / C;
+				++count;
+			}
+		}
+		
+		if (count >= D)
+		{
+			high = mid;
+		}
+		else
+		{
+			low = mid;
+		}
+	}
+	
+	cout << high;
+
+	return 0;
+}
